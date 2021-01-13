18f243af4742a073a2cdd2fc69c89c4f62a99577
Aditya <skilladi123@gmail.com>
2021-01-13 14:54:00 +0530

    add fonts

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './components/login'
+
+const getFonts = () => {
+  return Font.loadAsync({
+    'poppins-medium':require('./assets/fonts/Poppins-Medium.ttf'),
+    'poppins-semibold':require('./assets/fonts/Poppins-SemiBold.ttf')
+  });
+}
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded,setFontsLoaded] = useState(false);
+  
+  if(fontsLoaded){
+    return(
+      <Login />
+    );
+  }else{
+    render()
+    return(
+      <AppLoading 
+      startAsync={getFonts}
+      onFinish={()=>setFontsLoaded(true)}
+      />
+    )
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ components/login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet ,View ,Text} from 'react-native';
+
+export default function Home(){
+    return(
+        <View style = {styles.container}>
+            <Text style = {styles.titleText}>Login Text</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        padding:24
+    },
+    titleText:{
+        fontFamily:'poppins-medium',
+        fontSize:26,
+        fontWeight:"600",
+    }
+})
