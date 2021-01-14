2f0294249ef582cb08eb8430050b65f0e78a394e
tushx3 <tkushwaha70@gmail.com>
2021-01-14 19:14:08 +0530

    made home cards

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,39 +1,72 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
+import "react-native-gesture-handler";
+import React, { useState } from "react";
+import {
+  Button,
+  View,
+  Text,
+  StyleSheet,
+  TouchableNativeFeedback,
+} from "react-native";
 
-// Navigation Done but it has duplicating the stack
+const randomHexColor = () => {
+  return "#000000".replace(/0/g, function () {
+    return (~~(Math.random() * 16)).toString(16);
+  });
+};
 
 export default function HomeScreen({ navigation }) {
+  const [rippleColor, setRippleColor] = useState(randomHexColor());
+  const [rippleOverflow, setRippleOverflow] = useState(false);
   return (
-    <View style = {styles.container}>
-      <Text style={styles.texts}>Home Screen</Text>
-      <Button
-        title="Go to Earnings"
-        onPress={() => navigation.navigate('Earning')}
-      />
-      <Button
-        title="Go to Coupons"
-        onPress={() => navigation.navigate('Coupon')}
-      />
-      <Button
-        title="Go to LogIn"
-        onPress={() => navigation.navigate('LogIn')}
-      />
-      <Button
-        title="Go to SignUp"
-        onPress={() => navigation.navigate('SignUp')}
-      />
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+        <TouchableNativeFeedback
+          onPress={() => {
+            setRippleColor(randomHexColor());
+            setRippleOverflow(!rippleOverflow);
+          }}
+          background={TouchableNativeFeedback.Ripple(
+            rippleColor,
+            rippleOverflow
+          )}
+        >
+          <View style={styles.card}>
+            <Text style={styles.texts}>paisa</Text>
+          </View>
+        </TouchableNativeFeedback>
+        <TouchableNativeFeedback>
+          <View style={styles.card}>
+            <Text style={styles.texts}>paisa</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex: 1, alignItems: 'center', justifyContent: 'center' ,
+  wrapper: {
+    flexDirection: "row",
+    height: "20%",
+    width: "100%",
+    justifyContent: "space-evenly",
   },
-  texts:{
-    fontFamily:"Poppins-Medium",
-    fontSize:50,
-  }
-})
+  card: {
+    width: "45%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+    backgroundColor: "#000",
+    height: "100%",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  texts: {
+    fontFamily: "Poppins-Medium",
+    fontSize: 50,
+    color: "#fff",
+  },
+});
