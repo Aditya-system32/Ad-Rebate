b42e4fb552e6d5c4b1c288db2700ab80d59e537f
tushx3 <tkushwaha70@gmail.com>
2021-01-14 18:35:33 +0530

    added more fonts

--- App.js
+++ App.js
@@ -1,40 +1,39 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import EarningScreen from './Screens/Earning'
-import CouponScreen from './Screens/Coupon'
-import HomeScreen from './Screens/Home'
-import LogInScreen from './Screens/LogIn'
-import SignUpScreen from './Screens/SignUp'
-import VerificationScreen from './Screens/Verification'
-import * as Font from 'expo-font'
-import AppLoading from 'expo-app-loading'
-
+import "react-native-gesture-handler";
+import * as React from "react";
+import { Button, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import EarningScreen from "./Screens/Earning";
+import CouponScreen from "./Screens/Coupon";
+import HomeScreen from "./Screens/Home";
+import LogInScreen from "./Screens/LogIn";
+import SignUpScreen from "./Screens/SignUp";
+import VerificationScreen from "./Screens/Verification";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
 
 const Stack = createStackNavigator();
-const fetchfonts = ()=>{
+const fetchfonts = () => {
   return Font.loadAsync({
-    'Poppins-Medium':require('./assets/fonts/Poppins-Medium.ttf'), 
-    'Poppins-SemiBold':require('./assets/fonts/Poppins-SemiBold.ttf')
-  })
-}
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+  });
+};
 
-const fontLoadingError = () =>{
-  return(console.log('OOPS Font not Loaded'))
-}
-
-
+const fontLoadingError = () => {
+  return console.log("OOPS Font not Loaded");
+};
 
 function App() {
-  const [fontLoaded ,setFontLoaded] = React.useState(false)
+  const [fontLoaded, setFontLoaded] = React.useState(false);
 
-  if(!fontLoaded){
-    return (<AppLoading startAsync={fetchfonts}
-      onFinish={()=>setFontLoaded(true)}
-      onError={fontLoadingError}
-    />
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchfonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={fontLoadingError}
+      />
     );
   }
   return (
@@ -50,6 +49,5 @@ function App() {
     </NavigationContainer>
   );
 }
-
 
 export default App;
