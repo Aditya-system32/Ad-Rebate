e22d7d564343a8baff46d2a7b0f01feb57c88332
tushx3 <tkushwaha70@gmail.com>
2021-01-16 18:40:57 +0530

    finished login page

--- Screens/Home.js
+++ Screens/Home.js
@@ -13,7 +13,7 @@ import {
 export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-    <StatusBar backgroundColor='white' barStyle="dark-content" />
+      <StatusBar backgroundColor="black" barStyle="light-content" />
       <View style={styles.banner}>
         <Image></Image>
       </View>
@@ -21,12 +21,12 @@ export default function HomeScreen({ navigation }) {
         <Text style={styles.locationText}>Current location : Bhilai</Text>
       </View>
       <View style={styles.wrapper}>
-        <TouchableNativeFeedback onPress={() => navigation.navigate('Earning')}>
+        <TouchableNativeFeedback onPress={() => navigation.navigate("Earning")}>
           <View style={styles.card}>
             <Text style={styles.texts}>paisa</Text>
           </View>
         </TouchableNativeFeedback>
-        <TouchableNativeFeedback onPress={() => navigation.navigate('Coupon')}>
+        <TouchableNativeFeedback onPress={() => navigation.navigate("Coupon")}>
           <View style={styles.card}>
             <Text style={styles.texts}>paisa</Text>
           </View>
@@ -42,12 +42,15 @@ export default function HomeScreen({ navigation }) {
         </View>
       </View>
       <View style={styles.buttonWrapper}>
-        <TouchableNativeFeedback useForeground={true} onPress={() => navigation.navigate('LogIn')}>
+        <TouchableNativeFeedback
+          useForeground={true}
+          onPress={() => navigation.navigate("LogIn")}
+        >
           <View style={styles.loginButton}>
             <Text style={styles.loginButtonTitle}>Login</Text>
           </View>
         </TouchableNativeFeedback>
-        <TouchableNativeFeedback onPress={() => navigation.navigate('SignUp')}>
+        <TouchableNativeFeedback onPress={() => navigation.navigate("SignUp")}>
           <View style={styles.registerButton}>
             <Text style={styles.registerButtonTitle}>Register</Text>
           </View>
