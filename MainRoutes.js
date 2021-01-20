b966025f1c5189eb249be411bee8cd7a12d3248f
Aditya <skilladi123@gmail.com>
2021-01-20 19:25:32 +0530

    adding main route and clearing app.js

---
+++ MainRoutes.js
@@ -0,0 +1,43 @@
+import "react-native-gesture-handler";
+import * as React from "react";
+import { Button, View, Text, StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Navigation from './routes/Navigation'
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+
+const fetchfonts = () => {
+  return Font.loadAsync({
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+  });
+};
+
+const fontLoadingError = () => {
+  return console.log("OOPS Font not Loaded");
+};
+
+function MainRoute() {
+  const [fontLoaded, setFontLoaded] = React.useState(false);
+
+  
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchfonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={fontLoadingError}
+      />
+    );
+  }
+  return (
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
+  );
+}
+
+export default MainRoute;
