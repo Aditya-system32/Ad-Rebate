99052dda096dc02e1281e2538ed3322afd528f6c
tushx3 <tkushwaha70@gmail.com>
2021-01-23 21:13:55 +0530

    designed new verification page

--- Screens/Verification.js
+++ Screens/Verification.js
@@ -10,6 +10,7 @@ import {
   ActivityIndicator,
   Platform,
   StatusBar,
+  TouchableNativeFeedback,
 } from "react-native";
 import * as FirebaseRecaptcha from "expo-firebase-recaptcha";
 import * as firebase from "firebase";
@@ -33,9 +34,9 @@ export default function VerificationScreen({ route }) {
         <FirebaseRecaptcha.FirebaseRecaptchaVerifierModal
           ref={recaptchaVerifier}
           firebaseConfig={firebaseConfig}
+          title="Prove you are hooman!"
         />
-        <Text style={styles.title}>Firebase Authentication</Text>
-        <Text style={styles.text}>Enter the number</Text>
+        <Text style={styles.title}>Verify Mobile Number</Text>
         <TextInput
           style={styles.textInput}
           autoCompleteType="tel"
@@ -44,9 +45,7 @@ export default function VerificationScreen({ route }) {
           editable={!verificationId}
           value={route.params.paramKey}
         />
-        <Button
-          title={`${verificationId ? "Resend" : "Send"} Verification Code`}
-          disabled={!phoneNumber}
+        <TouchableNativeFeedback
           onPress={async () => {
             const phoneProvider = new firebase.auth.PhoneAuthProvider();
             try {
@@ -65,29 +64,31 @@ export default function VerificationScreen({ route }) {
               setVerifyInProgress(false);
             }
           }}
-        />
+        >
+          <View style={styles.sendButton}>
+            <Text style={styles.sendButtonText}>{`${
+              verificationId ? "Resend" : "Send"
+            } Verification Code`}</Text>
+          </View>
+        </TouchableNativeFeedback>
         {verifyError && (
           <Text style={styles.error}>{`Error: ${verifyError.message}`}</Text>
         )}
         {verifyInProgress && <ActivityIndicator style={styles.loader} />}
         {verificationId ? (
-          <Text style={styles.success}>
-            A verification code has been sent to your phone
-          </Text>
+          <Text style={styles.success}>verification code sent.</Text>
         ) : undefined}
-        <Text style={styles.text}>Enter verification code</Text>
         <TextInput
           ref={verificationCodeTextInput}
           style={styles.textInput}
           editable={!!verificationId}
-          placeholder="123456"
+          placeholder="Enter verification code"
+          placeholderTextColor="#EDEDED"
           onChangeText={(verificationCode) =>
             setVerificationCode(verificationCode)
           }
         />
-        <Button
-          title="Confirm Verification Code"
-          disabled={!verificationCode}
+        <TouchableNativeFeedback
           onPress={async () => {
             try {
               setConfirmError(undefined);
@@ -109,7 +110,13 @@ export default function VerificationScreen({ route }) {
               setConfirmInProgress(false);
             }
           }}
-        />
+        >
+          <View style={styles.confirmButton}>
+            <Text style={styles.confirmButtonText}>
+              Confirm Verification Code
+            </Text>
+          </View>
+        </TouchableNativeFeedback>
         {confirmError && (
           <Text style={styles.error}>{`Error: ${confirmError.message}`}</Text>
         )}
@@ -123,38 +130,100 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+    backgroundColor: "black",
+  },
+  sendButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    width: "80%",
+    borderRadius: 20,
+    height: 58,
+    backgroundColor: "#ffffff",
+    borderColor: "#8d8d8d",
+    borderStyle: "solid",
+    borderWidth: 1,
+    marginBottom: 44,
+  },
+  sendButtonText: {
+    color: "#000000",
+    fontSize: 16,
+    fontFamily: "Poppins-Medium",
+  },
+  confirmButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    width: "80%",
+    borderRadius: 20,
+    height: 58,
+    backgroundColor: "#000000",
+    borderColor: "#ffffff",
+    borderStyle: "solid",
+    borderWidth: 1,
+    marginBottom: 24,
+  },
+  confirmButtonText: {
+    color: "#ffffff",
+    fontSize: 16,
+    fontFamily: "Poppins-Medium",
   },
   content: {
     marginTop: 50,
   },
   title: {
-    marginBottom: 2,
+    marginBottom: 50,
     fontSize: 29,
     fontWeight: "bold",
+    color: "white",
+    fontFamily: "Poppins-Regular",
   },
   subtitle: {
     marginBottom: 10,
     opacity: 0.35,
     fontWeight: "bold",
+    color: "white",
+    fontFamily: "Poppins-Regular",
   },
   text: {
     marginTop: 30,
     marginBottom: 4,
+    color: "#EDEDED",
+    fontSize: 18,
+    fontFamily: "Poppins-Medium",
   },
   textInput: {
-    marginBottom: 8,
-    fontSize: 17,
-    fontWeight: "bold",
+    backgroundColor: "#1A1A1A",
+    borderColor: "#424242",
+    borderWidth: 1,
+    color: "#EDEDED",
+    fontSize: 18,
+    borderRadius: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    width: "80%",
+    height: 61,
+    alignSelf: "center",
+    justifyContent: "center",
+    fontSize: 18,
+    fontFamily: "Poppins-Regular",
+    marginBottom: 24,
   },
   error: {
-    marginTop: 10,
-    fontWeight: "bold",
-    color: "red",
+    marginTop: 0,
+    marginBottom: 24,
+    color: "#ff6666",
+    fontSize: 14,
+    fontFamily: "Poppins-Medium",
+    alignSelf: "center",
   },
   success: {
-    marginTop: 10,
-    fontWeight: "bold",
-    color: "blue",
+    marginTop: 0,
+    marginBottom: 24,
+    color: "#3edb65",
+    fontFamily: "Poppins-Medium",
+    fontSize: 14,
+    alignSelf: "center",
   },
   loader: {
     marginTop: 10,
