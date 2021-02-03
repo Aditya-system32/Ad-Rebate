16f7186b181a4f323784402b080725d1bad85d0c
tushx3 <tkushwaha70@gmail.com>
2021-02-03 18:16:33 +0530

    added get coupon page

---
+++ Screens/GetCoupon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function GetCoupon() {
+    useEffect(() => {
+        //api calls to server
+        
+    }, [])
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
