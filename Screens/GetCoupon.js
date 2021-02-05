f72671772a01f153471d97e4a4628f138a8cffd2
tushx3 <tkushwaha70@gmail.com>
2021-02-05 21:29:38 +0530

    clean

--- Screens/GetCoupon.js
+++ Screens/GetCoupon.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 import { db } from "../firebases";
+import { AuthContext } from "../routes/AuthProvider";
 
 export default function GetCoupon({ navigation, route }) {
   const id = route.params.paramKey;
   const [coupon, setCoupon] = useState(null);
+  const { user, setUserData, setBannerData, userData } = useContext(
+    AuthContext
+  );
   useEffect(() => {
     db.collection("ClientData")
       .doc(id)
@@ -21,11 +25,27 @@ export default function GetCoupon({ navigation, route }) {
       .catch(function (error) {
         console.log("Error getting documents: ", error);
       });
+    var current = new Date();
+    const x = current.getHours()+1+"-"+current.getMinutes()+"-"+current.getHours()+1;
+    const data = {
+      activeFrom: x,
+    };
   }, []);
   return (
     <View>
       <Text>{coupon ? coupon : "null"}</Text>
-      <Button title="HEllo"></Button>
+      <View style={styles.container}>
+        <View style={styles.coupon}></View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  coupon: {},
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
