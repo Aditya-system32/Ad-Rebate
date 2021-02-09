1d91b719f3f7008183f05e6d22a1e793f26e97cf
Aditya <skilladi123@gmail.com>
2021-02-09 16:16:03 +0530

    adding the splash screen

--- MainRoutes.js
+++ MainRoutes.js
@@ -3,29 +3,28 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./routes/Navigation";
 import * as Font from "expo-font";
-import AppLoading from "expo-app-loading";
 import { AuthContext } from "./routes/AuthProvider";
 import * as firebase from "firebase";
 import NavigationLogged from "./routes/NavigationLogged";
-const fetchfonts = () => {
-  return Font.loadAsync({
-    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
-    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
-    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
-  });
-};
+import { View, Text, StyleSheet } from "react-native";
+import { cos } from "react-native-reanimated";
 
-const fontLoadingError = () => {
-  return console.log("OOPS Font not Loaded");
-};
-
-function MainRoute() {
+export default function MainRoute() {
   const [fontLoaded, setFontLoaded] = React.useState(false);
-  const {
-    user,
-    setUser,
-  } = React.useContext(AuthContext);
+  const { user, setUser } = React.useContext(AuthContext);
   const [initializing, setInitializing] = React.useState(true);
+  const [appIsReady, setAppIsReady] = React.useState(false);
+
+  //Fonts Loading From Assests
+  React.useEffect(() => {
+    (async () =>
+      await Font.loadAsync({
+        "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+        "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+        "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+      }))();
+  }, []);
+
   const onAuthStateChanged = async (user) => {
     setUser(user);
     if (initializing) setInitializing(false);
@@ -36,13 +35,34 @@ function MainRoute() {
     return subscriber;
   }, []);
 
-  if (!fontLoaded && initializing) {
+  preventFromAutoHiding = async () => {
+    try {
+      await SplashScreen.preventAutoHideAsync();
+    } catch (e) {
+      console.warn(e);
+    }
+    prepareResources();
+  };
+
+  prepareResources = async () => {
+    try {
+      await performAPICalls();
+      await downloadAssets();
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      setFontLoaded(true);
+      setAppIsReady(true, async () => {
+        await SplashScreen.hideAsync();
+      });
+    }
+  };
+
+  if (initializing && !fontLoaded && !appIsReady) {
     return (
-      <AppLoading
-        startAsync={fetchfonts}
-        onFinish={() => setFontLoaded(true)}
-        onError={fontLoadingError}
-      />
+      <View style={styles.container}>
+        <Text style={styles.text}>SplashScreen!</Text>
+      </View>
     );
   }
   return (
@@ -52,4 +72,18 @@ function MainRoute() {
   );
 }
 
-export default MainRoute;
+async function performAPICalls() {}
+async function downloadAssets() {}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#aabbcc",
+  },
+  text: {
+    color: "white",
+    fontWeight: "bold",
+  },
+});
