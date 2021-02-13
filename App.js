fadfc2111a437fd6d9961a8f864bea3363ec3eac
Aditya <skilladi123@gmail.com>
2021-02-13 20:44:45 +0530

    adding the splash screen

--- App.js
+++ App.js
@@ -3,21 +3,21 @@ import * as React from "react";
 import MainRoutes from "./MainRoutes";
 import { AuthProvider } from "./routes/AuthProvider";
 import { LogBox } from "react-native";
-import { createStore } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers/index";
+//import { createStore } from "redux";
+//import thunk from "redux-thunk";
+//import rootReducer from "./reducers/index";
 LogBox.ignoreLogs(["Setting a timer"]);
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+//const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
 function App() {
   return (
     <AuthProvider>
-      <Provider store={store}>
-        <MainRoutes />
-      </Provider>
+      {/*<Provider store={store}>*/}
+      <MainRoutes />
+      {/*</Provider>*/}
     </AuthProvider>
   );
 }
--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -11,6 +11,7 @@ import {
   TextInput,
   Image,
   TouchableNativeFeedback,
+  Linking,
 } from "react-native";
 import BannerImages from "./BannerImages";
 import { Checkbox } from "react-native-paper";
@@ -58,6 +59,9 @@ export default function SignUpScreen({ navigation }) {
       setFlagCheckBox(true);
     }
   };
+  const handleTermsAndConditions = () => {
+    Linking.openURL("https://mural.com");
+  };
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
@@ -87,7 +91,9 @@ export default function SignUpScreen({ navigation }) {
             value={checkboxes}
             onValueChange={checkingCheckBoxes}
           />
-          <Text style={styles.forgetPass}>Agree to terms and conditions</Text>
+          <Text style={styles.forgetPass} onPress={handleTermsAndConditions}>
+            Agree to terms and conditions
+          </Text>
         </View>
         <View style={styles.loginWrapper}>
           <TouchableNativeFeedback
