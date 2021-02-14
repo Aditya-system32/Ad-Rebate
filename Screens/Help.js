399a1e24b69eb8e88f47aee761287af3fdc7c829
Aditya <skilladi123@gmail.com>
2021-02-14 18:47:10 +0530

    adding content in help page

--- Screens/Help.js
+++ Screens/Help.js
@@ -14,11 +14,17 @@ export default function HelpScreen({ navigation }) {
     {
       question: "How to Redeem Coupon",
       answer:
-        "Step 1 - Go to the home screen\nStep 2 - Select Barcode from top right corner\nStep 3 - Ask for the barcode from shopkeeper and scan the qr code\nStep 4 - Enter you bill and select the coupon you want to apply\nStep 5 - Show the Verified coupon and last bill to the shopkeeper and pay the amount give in the app ",
+        "Step 1 - Go to home screen\nStep 2 - Select Barcode from top right corner\nStep 3 - Ask for the barcode from shopkeeper and scan the qr code\nStep 4 - Enter you bill and select the coupon you want to apply\nStep 5 - Show the Verified coupon and last bill to the shopkeeper and pay the amount give in the app ",
     },
     {
-      question: "",
-      answer: "",
+      question: "How to Get Coupon",
+      answer:
+        "Step 1 - Go to home screen\nStep 2 -Select the category whose coupon you want\nStep 3 - Select the café and restaurant whose coupon you want\nStep 4 - Watch 3 Ads and answer one question\nStep 5 - You'll get coupon and the coupon will active after 1 hour",
+    },
+    {
+      question: "Why I Can't See My Coupon",
+      answer:
+        "Your Coupon Will activate after 1 hour , you can see your coupon in all section ",
     },
   ]);
   BackHandler.addEventListener("hardwareBackPress", function () {
